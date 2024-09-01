dd6f74d7fb48548d6ee1c857cef49554074f0a1d
babo24128 <babo24128@naver.com>
2024-09-01 14:53:44 +0900

    AI 시선 확인하기

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -4,20 +4,28 @@
 #include "ShooterAIController.h"
 #include "Kismet/GameplayStatics.h"
 
-void AShooterAIController::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-	MoveToActor(PlayerPawn, 200);
-}
-
 void AShooterAIController::BeginPlay()
 {
 	Super::BeginPlay();
 
 	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+}
 
-	SetFocus(PlayerPawn);
+void AShooterAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+	if (LineOfSightTo(PlayerPawn))
+	{
+		SetFocus(PlayerPawn);
+		MoveToActor(PlayerPawn, AcceptanceRadius);
+	}
+	else
+	{
+		ClearFocus(EAIFocusPriority::Gameplay);
+		StopMovement();
+	}
 
 }
