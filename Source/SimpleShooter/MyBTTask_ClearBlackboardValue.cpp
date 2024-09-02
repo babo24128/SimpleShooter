b02c7bde369f81bb010e02bcda511239391aa986
babo24128 <babo24128@naver.com>
2024-09-02 18:15:12 +0900

    C++로 커스텀 BTTasks 구성하기

---
+++ Source/SimpleShooter/MyBTTask_ClearBlackboardValue.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyBTTask_ClearBlackboardValue.h"
+
+UMyBTTask_ClearBlackboardValue::UMyBTTask_ClearBlackboardValue()
+{
+	NodeName = TEXT("Clear Blackborad Value");
+}
