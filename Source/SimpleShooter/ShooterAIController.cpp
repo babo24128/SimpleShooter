afaf0e8ade92b1c5cb75a0d325eb9497409fc992
babo24128 <babo24128@naver.com>
2024-09-02 16:29:18 +0900

    행동 트리 태스크 및 시퀀스

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -14,6 +14,8 @@ void AShooterAIController::BeginPlay()
 		RunBehaviorTree(AIBehavior);
 		APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+		GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
+
 	}
 }
 
