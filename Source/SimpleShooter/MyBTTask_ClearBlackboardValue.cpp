16d2632d7b2e83756d1555b6ae6cae254cac8ad1
babo24128 <babo24128@naver.com>
2024-09-04 20:16:31 +0900

    BTTasks 실행하기

--- Source/SimpleShooter/MyBTTask_ClearBlackboardValue.cpp
+++ Source/SimpleShooter/MyBTTask_ClearBlackboardValue.cpp
@@ -2,8 +2,18 @@
 
 
 #include "MyBTTask_ClearBlackboardValue.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
-UMyBTTask_ClearBlackboardValue::UMyBTTask_ClearBlackboardValue()
+UBTTask_ClearBlackboardValue::UBTTask_ClearBlackboardValue()
 {
 	NodeName = TEXT("Clear Blackborad Value");
+}
+
+EBTNodeResult::Type UBTTask_ClearBlackboardValue::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	Super::ExecuteTask(OwnerComp, NodeMemory);
+
+	OwnerComp.GetBlackboardComponent()->ClearValue(GetSelectedBlackboardKey());
+
+	return EBTNodeResult::Succeeded;
 }
--- Source/SimpleShooter/MyBTTask_ClearBlackboardValue.h
+++ Source/SimpleShooter/MyBTTask_ClearBlackboardValue.h
@@ -10,10 +10,13 @@
  * 
  */
 UCLASS()
-class SIMPLESHOOTER_API UMyBTTask_ClearBlackboardValue : public UBTTask_BlackboardBase
+class SIMPLESHOOTER_API UBTTask_ClearBlackboardValue : public UBTTask_BlackboardBase
 {
 	GENERATED_BODY()
 	
 public:
-	UMyBTTask_ClearBlackboardValue();
+	UBTTask_ClearBlackboardValue();
+
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 };
