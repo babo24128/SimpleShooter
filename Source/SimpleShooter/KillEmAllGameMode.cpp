94637f0ec411e6c7e3f0b4d65d0181c4a2316c2e
babo24128 <babo24128@naver.com>
2024-09-10 11:50:53 +0900

    C++에서 타이머 설정하기

--- Source/SimpleShooter/KillEmAllGameMode.cpp
+++ Source/SimpleShooter/KillEmAllGameMode.cpp
@@ -7,5 +7,10 @@ void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 {
 	Super::PawnKilled(PawnKilled);
 
-	UE_LOG(LogTemp, Warning, TEXT("A Pawn was Killed"));
+	APlayerController* PlayerController = Cast<APlayerController>(PawnKilled->GetController());
+
+	if (PlayerController != nullptr)
+	{
+		PlayerController->GameHasEnded(nullptr, false);
+	}
 }
